7fa35657a0076a5bf5ba52d0b9b53884cad16a5a
Sarah Allen <sarah@ultrasaurus.com>
2020-01-21 14:52:24 -0800

    wip - added static file plugin, static/sample.json

--- js/index.js
+++ js/index.js
@@ -1,23 +1,42 @@
-import wasm from '../crate/Cargo.toml'
+// import wasm from '../crate/Cargo.toml'
 
-try {
-  var greeting = wasm.hello("world");
-  console.log('hello:', greeting);
 
-  var arr = wasm.hello_array(4);
-  console.log("hello_array:", arr);
+// let result = wasm.doThing();
 
-  var h = wasm.hello_hash(3);
-  console.log("hello_hash:", h);
-}
-catch(error) {
-  console.error(error);
-}
+// 1. Async JS => Rust => JS
+// 2. HTTP Request from Rust
 
-try {
-  var greeting = wasm.hello();
-  console.log('hello:', greeting);
-}
-catch(error) {
-  console.log("hello requires a parameter (error caught successfully)\n", error);
-}
+fetch('http://localhost:1234/sample.json')
+  .then((response) => {
+    //debugger;
+    return response.json();
+  })
+  .then((myJson) => {
+    console.log(myJson);
+  });
+
+// localhost/:1 Access to fetch at 'http://localhost/' from origin 'http://localhost:1234' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+// index.js:9 Cross-Origin Read Blocking (CORB) blocked cross-origin response http://localhost/ with MIME type text/html. See https://www.chromestatus.com/feature/5629709824032768 for more details.
+
+
+// try {
+//   var greeting = wasm.hello("world");
+//   console.log('hello:', greeting);
+
+//   var arr = wasm.hello_array(4);
+//   console.log("hello_array:", arr);
+
+//   var h = wasm.hello_hash(3);
+//   console.log("hello_hash:", h);
+// }
+// catch(error) {
+//   console.error(error);
+// }
+
+// try {
+//   var greeting = wasm.hello();
+//   console.log('hello:', greeting);
+// }
+// catch(error) {
+//   console.log("hello requires a parameter (error caught successfully)\n", error);
+// }
