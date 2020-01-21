28e34d025765b05021c63aef954cc3238f85d89d
Sarah Allen <sarah@ultrasaurus.com>
2020-01-21 12:36:45 -0800

    recreate with fresh template

---
+++ crate/src/lib.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate cfg_if;
+
+#[macro_use]
+extern crate serde_derive;
+
+extern crate wasm_bindgen;
+extern crate js_sys;
+
+mod hello;
+mod hello_array;
+mod hello_hash;
+
+cfg_if! {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function to get better error messages if we ever panic.
+    if #[cfg(feature = "console_error_panic_hook")] {
+        extern crate console_error_panic_hook;
+        use console_error_panic_hook::set_once as set_panic_hook;
+    } else {
+        #[inline]
+        fn set_panic_hook() {}
+    }
+}
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
