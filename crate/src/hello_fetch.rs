37d21d3f2974bec294efb98cce2956ce5399f79b
Sarah Allen <sarah@ultrasaurus.com>
2020-01-21 17:13:11 -0800

    add rest of example

--- crate/src/hello_fetch.rs
+++ crate/src/hello_fetch.rs
@@ -1,42 +1,70 @@
-// use serde::{Deserialize, Serialize};
-// use wasm_bindgen::prelude::*;
-// use wasm_bindgen::JsCast;
-// use wasm_bindgen_futures::JsFuture;
-// use web_sys::{Request, RequestInit, RequestMode, Response};
-// use set_panic_hook;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+use crate::set_panic_hook;
 
-// // Called by our JS entry point to run the example
-// #[wasm_bindgen]
-// pub async fn hello_fetch(name: &str) -> Result<JsValue, JsValue> {
-//     set_panic_hook();
+/// A struct to hold some data from the github Branch API.
+///
+/// Note how we don't have to define every member -- serde will ignore extra
+/// data when deserializing
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Branch {
+    pub name: String,
+    pub commit: Commit,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Commit {
+    pub sha: String,
+    pub commit: CommitDetails,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CommitDetails {
+    pub author: Signature,
+    pub committer: Signature,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Signature {
+    pub name: String,
+    pub email: String,
+}
+
+// Called by our JS entry point to run the example
+#[wasm_bindgen]
+pub async fn hello_fetch(_name: &str) -> Result<JsValue, JsValue> {
+    set_panic_hook();
     
-//     let mut opts = RequestInit::new();
-//     opts.method("GET");
-//     opts.mode(RequestMode::Cors);
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
 
-//     let request = Request::new_with_str_and_init(
-//         "https://api.github.com/repos/rustwasm/wasm-bindgen/branches/master",
-//         &opts,
-//     )?;
+    let request = Request::new_with_str_and_init(
+        "https://api.github.com/repos/rustwasm/wasm-bindgen/branches/master",
+        &opts,
+    )?;
 
-//     request
-//         .headers()
-//         .set("Accept", "application/vnd.github.v3+json")?;
+    request
+        .headers()
+        .set("Accept", "application/vnd.github.v3+json")?;
 
-//     let window = web_sys::window().unwrap();
-//     let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
 
-//     // `resp_value` is a `Response` object.
-//     assert!(resp_value.is_instance_of::<Response>());
-//     let resp: Response = resp_value.dyn_into().unwrap();
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
 
-//     // Convert this other `Promise` into a rust `Future`.
-//     let json = JsFuture::from(resp.json()?).await?;
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
 
-//     // Use serde to parse the JSON into a struct.
-//     let branch_info: Branch = json.into_serde().unwrap();
+    // Use serde to parse the JSON into a struct.
+    let branch_info: Branch = json.into_serde().unwrap();
 
-//     // Send the `Branch` struct back to JS as an `Object`.
-//     Ok(JsValue::from_serde(&branch_info).unwrap())
-// }
+    // Send the `Branch` struct back to JS as an `Object`.
+    Ok(JsValue::from_serde(&branch_info).unwrap())
+}
 
